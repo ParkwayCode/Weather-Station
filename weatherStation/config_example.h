0b4c8af5d87a80fa989da12c18d94a76f02c4572
Vincent Amberger <ambergervincent2000@gmail.com>
2025-03-28 23:40:01 +0100

    Add example config and ignore real config.h

---
+++ weatherStation/config_example.h
@@ -0,0 +1,16 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// --- WiFi Settings ---
+const char* WIFI_SSID     = "YOUR_WIFI";
+const char* WIFI_PASSWORD = "password";
+
+// --- OpenWeather Settings ---
+const char* TOWN =         "CITY";
+const char* API_KEY =       "abcdefg12345";
+const char* UNITS =          "metric" ; // or "imperial"
+#define TIMEZONE      1         // UTC+1
+
+
+
+#endif
